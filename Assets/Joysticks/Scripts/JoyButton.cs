655937c46b47adf1d3b3b9a48ff144b1d0efd7d4
Laura <lauritaninetyseven@gmail.com>
2018-12-23 14:32:45 +0100

    second commit (completed copy)

---
+++ Assets/Joysticks/Scripts/JoyButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class JoyButton : MonoBehaviour, IPointerUpHandler, IPointerDownHandler {
+
+	[HideInInspector]
+	protected bool Pressed;
+
+	// Use this for initialization
+	void Start () {
+		Pressed = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public virtual void OnPointerDown(PointerEventData ped){
+		Pressed = true;	
+	}
+
+	public virtual void OnPointerUp(PointerEventData ped){
+		Pressed = false;	
+	}
+
+
+}
---
+++ Assets/Joysticks/Scripts/MovementController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour {
+
+	public VirtualJoystick leftJoystick;
+	public VirtualJoystick rightJoystick;
+	public JoyButton joyButton;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Translate (Vector3.forward * 0.5f * leftJoystick.Vertical ());
+		this.transform.Translate (Vector3.right * 0.5f * leftJoystick.Horizontal ());
+
+		this.transform.Rotate (Vector3.left * 2f * rightJoystick.Vertical ());
+		this.transform.Rotate (Vector3.up * 2f * rightJoystick.Horizontal (), Space.World);
+	}
+}
