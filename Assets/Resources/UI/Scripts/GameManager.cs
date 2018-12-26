16a44ea93d80848005134f5b499299d49b0f6a85
Laura <lauritaninetyseven@gmail.com>
2018-12-26 21:46:23 +0100

    settings menu finished

---
+++ Assets/Resources/UI/Scripts/GameManager.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    /*[DllImport("__Internal")]
+    private static extern int Load();
+    [DllImport("__Internal")]
+    private static extern void Save();*/
+    public GameObject uiManager;
+    private UIManager manager;
+
+    private void Awake()
+    {
+        if (!GameObject.FindGameObjectWithTag("UIManager"))
+        {
+            DontDestroyOnLoad(this);
+            GameObject ins = Instantiate(uiManager);
+            DontDestroyOnLoad(ins);
+        }
+        else
+        {
+            Destroy(this);
+        }
+
+        manager = GameObject.FindGameObjectWithTag("UIManager").GetComponent<UIManager>();
+
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        manager.scene = scene.name;
+        if (!scene.name.Contains("Level"))
+        {
+            manager.InstantiateLanguage();
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+
+        SceneManager.sceneLoaded += OnSceneLoaded;
+
+    }
+
+    //////////////////////////////////////////// MENU CONFIGURACION /////////////////////////////////////////////////////////////////////////////////
+
+    public void SetAntialiasing(GameObject obj)
+    {
+        Slider slider = obj.GetComponent<Slider>();
+        QualitySettings.antiAliasing = (int)Mathf.Pow(2, slider.value);
+    }
+
+    public void SetVolume(GameObject obj)
+    {
+        Slider slider = obj.GetComponent<Slider>();
+        AudioListener.volume = slider.value;
+    }
+
+    public void SetLanguage(int value)
+    {
+
+        manager = GameObject.FindGameObjectWithTag("UIManager").GetComponent<UIManager>();
+
+        Image l = GameObject.Find("languageOp").GetComponent<Image>();
+
+        if (value == 0) //es
+        {
+            manager.setLanguage("Spanish");
+            l.sprite = Resources.Load<Sprite>("UI/Images/es/español");
+        }
+        else //en
+        {
+            manager.setLanguage("English");
+            l.sprite = Resources.Load<Sprite>("UI/Images/en/english");
+        }
+
+    }
+
+
+    ////////////////////////////////////////////////////////////////////////////////////
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+
+    public void changeScene(string newScene)
+    {
+
+        manager = GameObject.FindGameObjectWithTag("UIManager").GetComponent<UIManager>();
+        manager.SceneChanged();
+        SceneManager.LoadScene(newScene);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/Resources/UI/Scripts/UIManager.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+    // Use this for initialization
+    public string lang = "Spanish";
+    public List<GameObject> es;
+    public List<GameObject> en;
+    public string scene;
+    private GameObject instance;
+
+    private void Awake()
+    {
+
+        scene = "main_menu"; // SE CAMBIA CADA VEZ QUE SE PASA A OTRA PANTALLA
+        InstantiateLanguage();
+
+    }
+
+    public void SceneChanged()
+    {
+        Destroy(GameObject.FindGameObjectWithTag("prefab"));
+    }
+
+    public void setLanguage(string v)
+    {
+        lang = v;
+    }
+
+    public string getLanguage()
+    {
+        return lang;
+    }
+
+    public void InstantiateLanguage() {
+
+        GameObject g;
+
+        switch (lang)
+        {
+
+            case "Spanish":
+                g = es.Find(x => x.name.Equals(scene + "_es"));
+                instance = Instantiate(g);
+                setParent(instance);
+                break;
+            case "English":
+                g = en.Find(x => x.name.Equals(scene + "_en"));
+                instance = Instantiate(g);
+                setParent(instance);
+                break;
+
+        }
+
+    }
+
+    private void setParent(GameObject instance)
+    {
+        GameObject parent = GameObject.FindGameObjectWithTag("canvas");
+        instance.transform.parent = parent.transform;
+        instance.transform.localPosition = Vector3.zero;
+        instance.transform.localScale = new Vector3(1, 1, 1);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+
+}
